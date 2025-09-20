6e92c5795cabb99c269180fb7c35e1fc48f55798
decard <mail@decard.space>
2025-09-21 07:01:32 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
 const port = process.env.PORT || 3000;
-const app = new Elysia().get("/", () => "Hello Elysia !!!").listen(port);
+const app = new Elysia().get("/", () => "Hello Elysia 222 !!!").listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
