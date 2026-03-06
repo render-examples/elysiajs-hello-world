4f900bc3516ce6aa45454fb476dc0d5fac35fbee
Eric Liu <ericyl.us@gmail.com>
2026-03-06 10:18:45 -0800

    Print APP_* vars

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { Elysia } from "elysia";
 
+const APP_NAME = process.env.APP_NAME || "Elysia";
+const APP_DESCRIPTION =
+  process.env.APP_DESCRIPTION ||
+  "Elysia is a web framework for building backend servers with Bun.";
+
+console.log(APP_NAME);
+console.log(APP_DESCRIPTION);
+
 const port = process.env.PORT || 3000;
 const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
